d280594271b8836182dec8a20f9d8daf227f69b0
Kamila <kamila.cerdeira.kc@gmail.com>
2021-05-25 17:59:29 -0300

    criacao do estilo global e fonte google

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <div className="App">
-      
+      <GlobalStyle />
     </div>
   );
 }
