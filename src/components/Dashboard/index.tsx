418692d0fff1234b5ea02484325b497eaf7b055f
Kamila <kamila.cerdeira.kc@gmail.com>
2021-05-31 10:11:06 -0300

    componente TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionTables } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTables />
         </Container>
     )
 }
