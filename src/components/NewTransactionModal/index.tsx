44a9cccf170de995285763bea2df33f77f42385e
Kamila <kamila.cerdeira.kc@gmail.com>
2021-06-11 18:31:49 -0300

    botoes de entrada e saida do form utilizando funcao js e propriedades dentro do styled component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg"
-import { Container } from "./style";
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+
+import { Container, TransactionTypeContainer, RadioBox } from "./style";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +12,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit')
+
+
     return (
         <Modal
             isOpen={isOpen}
@@ -31,6 +38,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="number"
                     placeholder="Valor"
                 />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={()=> { setType('deposit')}}
+                        isActive={ type == 'deposit' }
+                        activeColor="green"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox
+                        type="button"
+                        onClick={()=> { setType('withdraw')}}
+                        isActive={ type == 'withdraw'}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
                 <input
                     placeholder="Categoria"
                 />
